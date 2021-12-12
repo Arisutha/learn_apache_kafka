0b5e09d3f079f442a2829c7a13b1c9038f4cd66a
muhammad suta <wb-muhammads670675@antfin.com>
2021-12-12 09:25:07 +0700

    commint demo

---
+++ demo/src/main/java/com/demo/app/AppConfigs.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004‐2021 All Rights Reserved.
+ */
+package com.demo.app;
+
+/**
+ * @author muhammad suta (muhammad.suta@dana.id)
+ * @version $Id: AppConfigs.java, v 0.1 2021‐12‐11 05.24 muhammad suta (muhammad.suta@dana.id) Exp $$
+ */
+public class AppConfigs {
+}
---
+++ demo/src/main/java/com/demo/app/StorageDemo.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004‐2021 All Rights Reserved.
+ */
+package com.demo.app;
+
+/**
+ * @author muhammad suta (muhammad.suta@dana.id)
+ * @version $Id: StorageDemo.java, v 0.1 2021‐12‐11 05.27 muhammad suta (muhammad.suta@dana.id) Exp $$
+ */
+public class StorageDemo {
+}
